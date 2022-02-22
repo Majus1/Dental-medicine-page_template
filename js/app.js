ea828ad1be6ac94c02f6adb2d815ccbd840df738
Maj <59331721+Majus1@users.noreply.github.com>
2022-02-22 19:57:48 +0100

    Basic Owl carousel added

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Owl Carousel
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            768: {
+                items: 2,
+                nav: false
+            },
+            992: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+});
